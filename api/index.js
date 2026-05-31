3ab3008d6f377e542cb119b926a417c129df75ec
JCPrime01 <vanzokiller@gmail.com>
2026-05-31 10:47:21 -0300

    chore: shareCode YWFFGUFRR3S -> 0ENO79V6QLQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=YWFFGUFRR3S&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=0ENO79V6QLQ&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
